517739618f1ca35bf8a8f7d9d74d2b5c3c4ed1f2
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-11-24 13:23:54 -0600

    Added tests

--- 3-hard/js/seven-boom!.js
+++ 3-hard/js/seven-boom!.js
@@ -20,3 +20,7 @@ function sevenBoom(arr) {
         return "there is no 7 in the array"
     }
 }
+
+console.log(sevenBoom([1, 2, 3, 4, 5, 6, 7])); // ➞ "Boom!"
+console.log(sevenBoom([8, 6, 33, 100]));// ➞ "there is no 7 in the array"
+console.log(sevenBoom([2, 55, 60, 97, 86])); // ➞ "Boom!"
